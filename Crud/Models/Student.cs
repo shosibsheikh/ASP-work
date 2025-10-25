f25e5990bcb9e7341ee34c216ed5ba2f90be1d12
shosibsheikh <196762426+shosibsheikh@users.noreply.github.com>
2025-10-25 14:00:12 -0700

    Add files via upload

---
+++ Crud/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+         
+        [Column("StudentName", TypeName = ("varchar(100)"))]
+        [Required]
+        public string Name { get; set; }
+        [Column("StudentGender", TypeName = ("varchar(10)"))]
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public int? Age { get; set; }
+    }
+}
